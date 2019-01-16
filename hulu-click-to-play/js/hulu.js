af6551acd4c6e16f5e2a6a9d1c506a410b7d4326
Bobby McDonald <bobbymcwho@gmail.com>
2019-01-16 00:53:07 -0500

    Use a better selector

--- hulu-click-to-play/js/hulu.js
+++ hulu-click-to-play/js/hulu.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.isHuluPage === true){
-      const videoContainer = document.querySelector('.Player__container');
+      const videoContainer = document.querySelector('.video-player');
       const playbackButton = document.querySelector('.controls__playback-button');
       videoContainer.addEventListener('click', () => {
         playbackButton.click();
